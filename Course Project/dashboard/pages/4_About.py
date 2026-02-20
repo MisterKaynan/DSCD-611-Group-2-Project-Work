8f89efe93bf71633b648fc4b8922f5639b6f2d22
Kofi <etornamagbetornyo@gmail.com>
2026-02-20 14:09:42 +0000

    Fix formatting of developer names in 4_About.py

--- Course Project/dashboard/pages/4_About.py
+++ Course Project/dashboard/pages/4_About.py
@@ -72,8 +72,8 @@ It does not replace official meteorological or emergency warnings.
 
 ### Developed by  
 **Group 2:**
-Etornam Agbetonyo
-Charles Ahorsu
-Kwame Atua-Ntow
-Prince Obeng Nkoah        
+- Etornam Agbetonyo
+- Charles Ahorsu
+- Kwame Atua-Ntow
+- Prince Obeng Nkoah        
 """)
